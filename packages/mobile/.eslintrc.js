08e5969078ffcb100f90f520e1eb379ce5ddc71a
Nilomiranda <nilomiranda3@gmail.com>
2019-10-05 13:14:55 -0300

    sign in page

--- packages/mobile/.eslintrc.js
+++ packages/mobile/.eslintrc.js
@@ -1,4 +1,21 @@
-// module.exports = {
-//   root: true,
-//   extends: '@react-native-community',
-// };
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+  },
+};
--- packages/mobile/src/pages/Auth/SignIn.js
+++ packages/mobile/src/pages/Auth/SignIn.js
@@ -1,6 +1,76 @@
-import React from 'react';
-import {Text} from 'react-native';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+import { Text } from 'react-native';
 
-const SignIn = () => <Text>Hello from sign in</Text>;
+const MainContainer = styled.View`
+  align-items: center;
+  /* justify-content: space-between; */
+  padding: 50px 40px;
+  flex: 1;
+`;
+
+const TextInput = styled.TextInput`
+  /* height: 40px; */
+  color: red;
+  box-shadow: 5px 4px 15px rgba(0, 0, 0, 0.5);
+  border-radius: 6px;
+  background: #fff;
+  padding: 16px 10px;
+  align-self: stretch;
+  margin-bottom: 30px;
+`;
+
+const InputLabel = styled.Text`
+  color: #222;
+  font-size: 18px;
+  font-weight: normal;
+  margin-bottom: 8px;
+  text-align: center;
+  align-self: flex-start;
+`;
+
+const SubmitButton = styled.TouchableOpacity`
+  background: #394a6d;
+  border-radius: 6px;
+  padding: 12px;
+  align-self: stretch;
+`;
+
+const SubmitButtonLabel = styled.Text`
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: bold;
+  align-self: stretch;
+  text-align: center;
+`;
+
+const TextLink = styled.Text`
+  color: #3c9d9b;
+  font-size: 14px;
+  font-weight: bold;
+  align-self: flex-start;
+  margin-top: 25px;
+`;
+
+class SignIn extends Component {
+  render() {
+    return (
+      <MainContainer>
+        <InputLabel>Email</InputLabel>
+        <TextInput placeholder="you@domain.com" />
+
+        <InputLabel>Password</InputLabel>
+        <TextInput placeholder="********" />
+
+        <SubmitButton>
+          <SubmitButtonLabel>Sign In</SubmitButtonLabel>
+        </SubmitButton>
+
+        <TextLink>Forgot password? Click here</TextLink>
+      </MainContainer>
+    );
+  }
+}
 
 export default SignIn;
