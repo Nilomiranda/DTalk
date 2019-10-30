561df46adb0e8ff9fb1aa0b8710a67f032b3ce6b
Nilomiranda <nilomiranda3@gmail.com>
2019-10-29 21:51:04 -0300

    done with text post component

--- packages/mobile/src/components/TextPost.js
+++ packages/mobile/src/components/TextPost.js
@@ -1,36 +1,71 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
+/* eslint-disable global-require */
+/* eslint-disable react/prefer-stateless-function */
+import React, {Component} from 'react';
+import {Text} from 'react-native';
 import styled from 'styled-components/native';
 
 const MainContainer = styled.View`
   background: #eee;
   border-radius: 15px;
+  padding: 25px;
+  margin: 10px 0;
 `;
 
 const PostHeader = styled.View`
-  border: 1px solid #ddd;
+  border-bottom-width: 1px;
+  /* border-bottom-style: solid; */
+  border-bottom-color: #ddd;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-bottom: 13px;
 `;
 
-const AuthorImg = styled.Image``;
+const AuthorImg = styled.Image`
+  width: 45px;
+  height: 45px;
+  border-radius: 22px;
+`;
 
-const Metadata = styled.View``;
+const Metadata = styled.View`
+  margin-left: 15px;
+`;
 
-const Author = styled.Text``;
+const Author = styled.Text`
+  font-size: 16px;
+  color: #666;
+`;
 
-const PostDate = styled.Text``;
+const PostDate = styled.Text`
+  font-size: 13px;
+  color: #bbb;
+`;
+
+const Content = styled.Text`
+  color: #444;
+  font-weight: bold;
+  font-size: 14px;
+  margin-top: 15px;
+`;
 
 class TextPost extends Component {
   render() {
     return (
       <MainContainer>
         <PostHeader>
-          <AuthorImg source={{ uri: '../assets/img/avatar.jpeg' }} />
+          <AuthorImg source={require('../assets/img/avatar.jpeg')} />
           <Metadata>
             <Author>Daniel Carter</Author>
             <PostDate>1 hour ago</PostDate>
           </Metadata>
         </PostHeader>
-        <Text>Hello</Text>
+        <Content>
+          Eos soleat doming dolorem id, no eripuit inermis albucius usu, est
+          erat eirmod ea. Veritus conceptam rationibus his ei, vim in errem
+          volutpat salutatus. Te error feugiat sea, tamquam consectetuer per te.
+          Ipsum deseruisse cum ut, tation consul dignissim sit ut. Facer essent
+          viderer vix ne, ei vel exerci tractatos tincidunt.
+        </Content>
       </MainContainer>
     );
   }
--- packages/mobile/src/pages/Social/NewsFeed.js
+++ packages/mobile/src/pages/Social/NewsFeed.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
+import styled from 'styled-components/native';
 
 /** custom components */
 import TextPost from '../../components/TextPost';
 
-const NewsFeed = () => <TextPost />;
+const FeedContainer = styled.ScrollView`
+  padding: 10px 20px;
+`;
+
+const NewsFeed = () => (
+  <FeedContainer>
+    <TextPost />
+    <TextPost />
+    <TextPost />
+    <TextPost />
+    <TextPost />
+  </FeedContainer>
+);
 
 export default NewsFeed;
