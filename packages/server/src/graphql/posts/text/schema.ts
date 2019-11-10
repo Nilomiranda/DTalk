810db033a1bacd589738acba077de1c217c28cbe
Nilomiranda <nilomiranda3@gmail.com>
2019-11-10 16:59:28 -0300

    protecting posts query from not logged users

--- packages/server/src/graphql/posts/text/schema.ts
+++ packages/server/src/graphql/posts/text/schema.ts
@@ -6,13 +6,15 @@ import {
 } from 'graphql';
 import User from '../../users/schema';
 import { Prisma } from '../../../prisma/generated/prisma-client';
+import isUserLogged from '../../../middlewares/auth';
 
 const TextPost = new GraphQLObjectType({
   name: 'TextPost',
   fields: {
     id: {
       type: GraphQLID,
-      resolve(parent) {
+      resolve(parent, args, context) {
+        isUserLogged(context);
         return parent.id;
       },
     },
@@ -21,13 +23,14 @@ const TextPost = new GraphQLObjectType({
       // args: { id: { type: GraphQLString } },
       resolve(parent, args, context) {
         console.log('TCL: resolve -> args', args.id);
+        isUserLogged(context);
         return context.prisma.textPost({ id: parent.id }).postedBy();
       },
     },
     content: {
       type: GraphQLNonNull(GraphQLString),
-      resolve(parent, args, context: Prisma | any) {
-        console.log('TCL: resolve -> content -> parent', parent);
+      resolve(parent, args, context) {
+        isUserLogged(context);
         return parent.content;
       },
     },
