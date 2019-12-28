2dd29ed6066d81fea4f8ae51cc5c7340ff8d2ca2
Nilomiranda <nilomiranda3@gmail.com>
2019-12-27 21:15:48 -0300

    fix multiple android connections

--- packages/mobile/src/config/ReactotronConfig.js
+++ packages/mobile/src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const tron = Reactotron.configure()
   // .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure() // controls connection & communication settings
+  .configure({ host: '192.168.0.37' }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
+  .setAsyncStorageHandler(AsyncStorage)
   .connect(); // let's connect!
 
 console.tron = tron;
