0f7b8df0061e6964d1264faa653d431aacf71f64
Nilomiranda <nilomiranda3@gmail.com>
2019-12-27 21:03:59 -0300

    platform check for correct api url

--- packages/mobile/src/config/relayEnvironment.js
+++ packages/mobile/src/config/relayEnvironment.js
@@ -1,12 +1,22 @@
 import { Environment, Network, RecordSource, Store } from 'relay-runtime';
 import AsyncStorage from '@react-native-community/async-storage';
 import { installRelayDevTools } from 'relay-devtools';
+import { Platform } from 'react-native';
 
 async function fetchQuery(operation, variables) {
   const token = await AsyncStorage.getItem('SESSION_TOKEN');
+  let API_URL = '';
   console.tron.log('TCL: fetchQuery -> token', token);
 
-  return fetch('http://10.0.2.2:3333/graphiql', {
+  const platform = Platform.OS;
+
+  if (platform === 'ios') {
+    API_URL = 'http://localhost:3333/graphiql';
+  } else {
+    API_URL = 'http://10.0.2.2:3333/graphiql';
+  }
+
+  return fetch(API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
